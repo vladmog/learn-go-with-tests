4436af529247218845680f21f0eb4b4201544930
vladmog <vladmog@gmail.com>
2023-12-06 17:38:17 -0800

    implemented and tested an interface

--- structs/shapes.go
+++ structs/shapes.go
@@ -15,12 +15,21 @@ type Circle struct {
 	Radius float64
 }
 
-type Shape interface {
-	Area() float64
-}
-
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+type Shape interface {
+	Area() float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
